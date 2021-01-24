b1e31e1d711b9683ae1aed9b72e058a391e86fc5
beercafeguy <he.thuwal08@gmail.com>
2021-01-24 09:48:28 +0530

    added score board for demo of sorted array

--- arrays/src/main/java/com/beercageguy/ds/array/GameEntry.java
+++ arrays/src/main/java/com/beercageguy/ds/array/GameEntry.java
@@ -1,2 +1,38 @@
-package com.beercageguy.ds.array;public class GameEntry {
+package com.beercageguy.ds.array;
+
+public class GameEntry {
+    private String name;
+    private int score;
+
+    public GameEntry() {
+    }
+
+    public GameEntry(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "GameEntry{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
--- arrays/src/main/java/com/beercageguy/ds/array/ScoreBoard.java
+++ arrays/src/main/java/com/beercageguy/ds/array/ScoreBoard.java
@@ -1,2 +1,31 @@
-package com.beercageguy.ds.array;public class ScoreBoard {
+package com.beercageguy.ds.array;
+
+public class ScoreBoard {
+
+    private final int capacity;
+
+    private int numEntries=0;
+
+    private GameEntry[] entries;
+
+    public ScoreBoard(int capacity) {
+        this.capacity=capacity;
+        entries=new GameEntry[capacity];
+    }
+
+    public void add(GameEntry entry){
+        int newScore=entry.getScore();
+        if(numEntries<capacity || newScore>entries[numEntries-1].getScore()){ // case when new score will be entertained
+            if(numEntries < capacity){
+                numEntries++;
+            }
+
+            int i=numEntries -1;
+            while(i > 0 && entries[i].getScore() < newScore){
+                entries[i]=entries[i-1];
+                i--;
+            }
+            entries[i]=entry;
+        }
+    }
 }
