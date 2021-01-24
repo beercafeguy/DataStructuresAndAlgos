54dc10cdaa323f6947ffa68daa26fe4752578f0a
beercafeguy <hemchandra@outlook.com>
2021-01-24 12:08:02 +0530

    Added insertion sort for string array

--- arrays/src/main/java/com/beercageguy/ds/array/InsertionSortDemo.java
+++ arrays/src/main/java/com/beercageguy/ds/array/InsertionSortDemo.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 
 public class InsertionSortDemo {
     public static void main(String[] args) {
+        System.out.println("************ INT Sort ************");
         int[] randomArray={1,52,3,22,0,6,45,4};
         System.out.println(Arrays.toString(randomArray));
         System.out.println(Arrays.toString(sort(randomArray)));
+
+        System.out.println("************ String Sort ************");
+        String[] randomStrArray={"Hem","Ajay","Mohan","Bhagya"};
+        System.out.println(Arrays.toString(randomStrArray));
+        System.out.println(Arrays.toString(sort(randomStrArray)));
     }
 
     public static int[] sort(int[] arr){
@@ -15,6 +21,20 @@ public class InsertionSortDemo {
             for(int j =0;j<i;j++){
                 if(arr[j] > arr[j+1]){
                     int tmp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static String[] sort(String[] arr){
+        int size=arr.length-1;
+        for(int i=size;i>0;i--){
+            for(int j =0;j<i;j++){
+                if(arr[j] .compareTo(arr[j+1]) > 0){
+                    String tmp=arr[j+1];
                     arr[j+1]=arr[j];
                     arr[j]=tmp;
                 }
