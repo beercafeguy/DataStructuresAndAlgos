4866b77bc746bbafe1f85cc82287093daf184957
beercafeguy <hemchandra@outlook.com>
2021-05-09 14:24:01 +0530

    Solutions for first non rep char

--- arrays/src/main/java/com/beercageguy/ds/array/RotateArray.java
+++ arrays/src/main/java/com/beercageguy/ds/array/RotateArray.java
@@ -32,4 +32,17 @@ public class RotateArray {
         }
         return arr;
     }
+
+    static int[] rotLeftExtraSpace(int[] a, int d) {
+        int n = a.length;
+        int[] rotArray = new int[n];
+
+        for (int oldIndex = 0; oldIndex < n; oldIndex++) {
+            int newIndex = (oldIndex + n - d) % n;
+            rotArray[newIndex] = a[oldIndex];
+        }
+
+        return rotArray;
+
+    }
 }
