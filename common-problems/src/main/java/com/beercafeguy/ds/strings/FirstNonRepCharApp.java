17e3f0808be4888f2e046583dec7d1157fd8a40e
beercafeguy <he.thuwal08@gmail.com>
2021-05-09 14:10:13 +0530

    Added array method for finding first non repeating char

--- common-problems/src/main/java/com/beercafeguy/ds/strings/FirstNonRepCharApp.java
+++ common-problems/src/main/java/com/beercafeguy/ds/strings/FirstNonRepCharApp.java
@@ -11,8 +11,15 @@ public class FirstNonRepCharApp {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String input = bufferedReader.readLine();
         System.out.println(input);
-        
-        char[] chars= input.toLowerCase().toCharArray();
+
+        Character usingMap=getFirstNonRepeat(input);
+        int usingArrIndex=getFirstNonRepeatUsingArray(input);
+        System.out.println((usingMap==null)? "-1" : usingMap);
+        System.out.println("Index :"+usingArrIndex);
+    }
+
+    private static Character getFirstNonRepeat(String string){
+        char[] chars= string.toLowerCase().toCharArray();
         HashMap<Character,Integer> map=new HashMap<>();
         for (char c:chars) {
             if(map.containsKey(c)){
@@ -24,10 +31,30 @@ public class FirstNonRepCharApp {
         Character output=null;
         for (char c:chars) {
             if(map.get(c)==1){
-                output=c;
+                return c;
+            }
+        }
+        return null;
+    }
+
+    private static int getFirstNonRepeatUsingArray(String str){
+        int[] holder=new int[256];
+       String string= str.toLowerCase();
+
+        for (int i = 0; i < string.length(); i++){
+            if(string.charAt(i)!=' '){
+                holder[string.charAt(i)]++;
+            }
+        }
+
+
+        int j, index = -1;
+        for (j = 0; j < string.length(); j++) {
+            if (holder[string.charAt(j)] == 1) {
+                index = j;
                 break;
             }
         }
-        System.out.println((output==null)? "-1" : output);
+        return index;
     }
 }
