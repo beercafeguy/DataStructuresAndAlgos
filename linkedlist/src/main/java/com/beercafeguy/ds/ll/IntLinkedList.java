266d717fe0acfa980cbc6a3f183bcccf031e5864
beercafeguy <hemchandra@outlook.com>
2021-05-31 22:59:09 +0530

    double pointer method for finding middle of linkedlist

--- linkedlist/src/main/java/com/beercafeguy/ds/ll/IntLinkedList.java
+++ linkedlist/src/main/java/com/beercafeguy/ds/ll/IntLinkedList.java
@@ -6,7 +6,7 @@ public class IntLinkedList {
 
     public static void main(String[] args) {
 
-        IntLinkedList ill=new IntLinkedList();
+        IntLinkedList ill = new IntLinkedList();
         System.out.println("Adding elements");
         ill.addElements();
         System.out.println("Elements added.");
@@ -17,67 +17,80 @@ public class IntLinkedList {
         System.out.println("Delete operation");
         ill.delete(4);
         ill.print();
+        ill.insert(5);
+        ill.insert(6);
+        ill.insert(7);
+        ill.findMiddle();
+        ill.print();
 
     }
 
-    public void insert(int input){
-        Node inputNode=new Node(input);
-        if(head==null){
-            head=inputNode;
-        }else{
-            Node last=head;
-            while(last.next!=null){
-                last=last.next;
+    public int findMiddle(){
+        Node singleStep=head;
+        Node doubleStep=head;
+        while(doubleStep!=null && doubleStep.next!=null){
+            singleStep=singleStep.next;
+            doubleStep=doubleStep.next.next;
+        }
+        System.out.println("Middle element is "+singleStep.data);
+        return singleStep.data;
+    }
+
+    public void insert(int input) {
+        Node inputNode = new Node(input);
+        if (head == null) {
+            head = inputNode;
+        } else {
+            Node last = head;
+            while (last.next != null) {
+                last = last.next;
             }
-            last.next=inputNode;
+            last.next = inputNode;
         }
     }
 
-    public void delete(int key){
-        Node current=head;
-        Node prev=null;
-        if(head!=null){
-            if (head.data==key){
-                head=head.next;
-            }else{
-                while(current!=null && current.data!=key){
-                    prev=current;
-                    current=current.next;
+    public void delete(int key) {
+        Node current = head;
+        Node prev = null;
+        if (head != null) {
+            if (head.data == key) {
+                head = head.next;
+            } else {
+                while (current != null && current.data != key) {
+                    prev = current;
+                    current = current.next;
                 }
-                if(current!=null){
-                    prev.next=current.next;
+                if (current != null) {
+                    prev.next = current.next;
                 }
             }
         }
     }
 
-    public void print(){
-        Node last=head;
-        if(head!=null){
-            while(last.next!=null){
-                System.out.println(last.data);
-                last=last.next;
-            }
+    public void print() {
+        Node last = head;
+        while (last!= null) {
             System.out.println(last.data);
+            last = last.next;
         }
     }
 
-    public Node addElements(){
-        this.head=new Node(1);
-        Node second=new Node(2);
-        Node third=new Node(3);
-        head.next=second;
-        head.next.next=third;
+    public Node addElements() {
+        this.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        head.next = second;
+        head.next.next = third;
         return this.head;
     }
 
-    class Node{
+    class Node {
         int data;
         Node next;
 
-        public Node(int data){
-            this.data=data;
-            this.next=null;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
 }
