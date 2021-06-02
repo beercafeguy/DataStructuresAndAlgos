e82cc2ee3e4bd822e20ddfd8bc650abffb4a2a85
beercafeguy <hemchandra@outlook.com>
2021-06-02 22:48:44 +0530

    found loop in linkedList using two pointers

--- linkedlist/src/main/java/com/beercafeguy/ds/ll/FindLoopLinkedListApp.java
+++ linkedlist/src/main/java/com/beercafeguy/ds/ll/FindLoopLinkedListApp.java
@@ -6,58 +6,81 @@ import java.util.Set;
 public class FindLoopLinkedListApp {
 
     public static void main(String[] args) {
-        FindLoopLinkedListApp app=new FindLoopLinkedListApp();
-        Node loopedHead=app.createLoopedList();
-        Node nonLoopedHead=app.createNonLoopedList();
-        System.out.println("Is Looped: "+app.hasLoop(loopedHead));
-        System.out.println("Is Looped: "+app.hasLoop(nonLoopedHead));
+        FindLoopLinkedListApp app = new FindLoopLinkedListApp();
+        Node loopedHead = app.createLoopedList();
+        Node nonLoopedHead = app.createNonLoopedList();
+        System.out.println("Is Looped: " + app.hasLoop(loopedHead));
+        System.out.println("Is Looped: " + app.hasLoop(nonLoopedHead));
+        System.out.println("Is Looped two pointer: " + app.findLoopTwoPointer(loopedHead));
+        System.out.println("Is Looped two pointer: " + app.findLoopTwoPointer(nonLoopedHead));
     }
-    public boolean hasLoop(Node head){
-        Set<Node> nodes=new HashSet<>();
-        Node last=head;
-        while(last!=null){
-            if(nodes.contains(last)){
+
+    public boolean findLoopTwoPointer(Node head) {
+        Node slow = head;
+        Node fast = head;
+        boolean isLooped = false;
+        while (fast!= null) {
+            fast = fast.next;
+            if (fast!=null) {
+                fast=fast.next;
+                slow = slow.next;
+            }
+            if(fast==slow){
+                isLooped=true;
+                break;
+            }
+        }
+        return isLooped;
+    }
+
+
+    public boolean hasLoop(Node head) {
+        Set<Node> nodes = new HashSet<>();
+        Node last = head;
+        while (last != null) {
+            if (nodes.contains(last)) {
                 return true;
             }
             nodes.add(last);
-            last=last.next;
+            last = last.next;
         }
         return false;
     }
 
-    private Node createNonLoopedList(){
-        Node first=new Node(1);
-        Node second=new Node(2);
-        Node third=new Node(3);
-        Node fourth=new Node(4);
-        Node fifth=new Node(5);
-        first.next=second;
-        second.next=third;
-        third.next=fourth;
-        fourth.next=fifth;
+    private Node createNonLoopedList() {
+        Node first = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
         return first;
     }
 
-    private Node createLoopedList(){
-        Node first=new Node(1);
-        Node second=new Node(2);
-        Node third=new Node(3);
-        Node fourth=new Node(4);
-        Node fifth=new Node(5);
-        first.next=second;
-        second.next=third;
-        third.next=fourth;
-        fourth.next=fifth;
-        fifth.next=second;
+    private Node createLoopedList() {
+        Node first = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = second;
         return first;
     }
-    private class Node{
+
+    private class Node {
         int data;
         Node next;
 
         public Node(int data) {
             this.data = data;
-            this.next=null;
+            this.next = null;
         }
     }
 }
