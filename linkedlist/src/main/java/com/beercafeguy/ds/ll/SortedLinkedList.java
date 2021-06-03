1cd3963825ec7922c62d5c3f2d2b03e51229f468
beercafeguy <hemchandra@outlook.com>
2021-06-03 22:16:47 +0530

    sorted linked list

---
+++ linkedlist/src/main/java/com/beercafeguy/ds/ll/SortedLinkedList.java
@@ -0,0 +1,57 @@
+package com.beercafeguy.ds.ll;
+
+public class SortedLinkedList {
+
+    Node head;
+    private int size;
+
+    public Node insert(int input){
+        Node inputNode=new Node(input);
+        if(head==null){
+            head=inputNode;
+        }else{
+            Node prev=null;
+            Node current=head;
+            while(current!=null && current.data<=input){
+                prev=current;
+                current=current.next;
+            }
+            if(current==null){
+                prev.next=inputNode;
+            }else if(prev==null){
+                inputNode.next=current;
+                head=inputNode;
+            }else{
+                prev.next=inputNode;
+                inputNode.next=current;
+            }
+        }
+        size++;
+        return head;
+    }
+
+    public void print(){
+        Node last=head;
+        while(last!=null){
+            System.out.print(last.data+" ");
+            last=last.next;
+        }
+        System.out.println();
+    }
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            next=null;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    '}';
+        }
+    }
+}
