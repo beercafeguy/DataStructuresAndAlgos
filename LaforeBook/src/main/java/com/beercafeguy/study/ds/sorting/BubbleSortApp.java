357367646c8180e3ca604050627308b2d265ccf2
beercafeguy <hemchandra@outlook.com>
2021-06-08 22:28:59 +0530

    updated sorted programs

--- LaforeBook/src/main/java/com/beercafeguy/study/ds/sorting/BubbleSortApp.java
+++ LaforeBook/src/main/java/com/beercafeguy/study/ds/sorting/BubbleSortApp.java
@@ -12,17 +12,19 @@ public class BubbleSortApp {
         Integer[] inputArray={10,2,-1,0,55,22,110};
         System.out.println("Unsorted Array: "+ Arrays.toString(inputArray));
 
-        for(int i=0;i<=inputArray.length-1;i++){
-            int selectedIndex=i;
-            for(int j=i+1;j<inputArray.length-1;j++){
-                if(inputArray[selectedIndex]>inputArray[j]){
-                    selectedIndex=j;
+        // loop starting from 0 because we are assuming the at the start
+        // we have an array of size 1 and because its size = 1 so it will always be
+        // sorted
+        for(int i=inputArray.length;i>=0;i--){
+            for(int j=0;j<i-1;j++){
+                if(inputArray[j]>inputArray[j+1]){
+                    int tmp=inputArray[j+1];
+                    inputArray[j+1]=inputArray[j];
+                    inputArray[j]=tmp;
                 }
             }
-            int tmp=inputArray[selectedIndex];
-            inputArray[selectedIndex]=inputArray[i];
-            inputArray[i]=tmp;
         }
+
         System.out.println("Sorted Array: "+ Arrays.toString(inputArray));
     }
 }
--- LaforeBook/src/main/java/com/beercafeguy/study/ds/sorting/SelectionSortApp.java
+++ LaforeBook/src/main/java/com/beercafeguy/study/ds/sorting/SelectionSortApp.java
@@ -12,6 +12,19 @@ public class SelectionSortApp {
         Integer[] inputArray={10,2,-1,0,55,22,110};
         System.out.println("Unsorted Array: "+ Arrays.toString(inputArray));
 
+        for(int i=0;i<=inputArray.length-1;i++){
+            int selectedIndex=i;
+            for(int j=i+1;j<inputArray.length-1;j++){
+                if(inputArray[selectedIndex]>inputArray[j]){
+                    selectedIndex=j;
+                }
+            }
+
+            //swap the minimum from remaining and put here
+            int tmp=inputArray[selectedIndex];
+            inputArray[selectedIndex]=inputArray[i];
+            inputArray[i]=tmp;
+        }
 
         System.out.println("Sorted Array: "+ Arrays.toString(inputArray));
     }
