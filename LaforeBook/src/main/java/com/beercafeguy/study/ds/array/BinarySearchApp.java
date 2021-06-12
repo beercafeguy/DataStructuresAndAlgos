41f87f7fcb6c282465ffad6cdb5546a5f8235d85
beercafeguy <hemchandra@outlook.com>
2021-06-12 17:30:47 +0530

    simple BinarySearchApp search using recursion

--- LaforeBook/src/main/java/com/beercafeguy/study/ds/array/BinarySearchApp.java
+++ LaforeBook/src/main/java/com/beercafeguy/study/ds/array/BinarySearchApp.java
@@ -4,7 +4,26 @@ public class BinarySearchApp {
 
     public static void main(String[] args) {
         //System.out.println(binarySearchLoop(22));
-        System.out.println(binarySearchLoop(155));
+        //System.out.println(binarySearchLoop(155));
+        System.out.println(binarySearchRecursion(0,6,155));
+        // System.out.println(binarySearchLoop(155));
+
+
+    }
+
+    private static boolean binarySearchRecursion(int lower,int upper,int key) {
+        int[] input={1,10,20,44,55,123,155};
+        int currentIndex=(lower+upper)/2;
+        if(input[currentIndex]==key){
+            return true;
+        }else if (lower>upper){
+            return false;
+        }else if(input[currentIndex]> key){
+            return binarySearchRecursion(lower,currentIndex-1,key);
+        }else if(input[currentIndex]<key){
+            return binarySearchRecursion(currentIndex+1,upper,key);
+        }
+        return false;
     }
 
     private static boolean binarySearchLoop(int key) {
