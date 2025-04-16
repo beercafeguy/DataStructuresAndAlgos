28cba8392ff8a73625dd43223406e41e1f0b1838
beercafeguy <hemchandra@outlook.com>
2025-04-16 21:33:53 +0530

    fibonachi series in iterative way

--- recursion/basics.py
+++ recursion/basics.py
@@ -1,11 +1,9 @@
 
 
-# fibonacci series
-
-
+# factorial
 # loop version
 
-def fib_loop(num):
+def fact_loop(num):
     result = 1
     while num > 1:
         result = result * num
@@ -13,11 +11,45 @@ def fib_loop(num):
     return result
 
 
-def fib_recursive(n):
+# recursive
+def fact_recursive(n):
     if n == 1:
         return 1
     else:
-        return n * fib_recursive(n-1)
-#print(fib_loop(4))
+        return n * fact_recursive(n-1)
+#print(fact_loop(4))
 
-print(fib_recursive(4))
+print(fact_recursive(4))
+
+
+# print fibonacci series
+
+# loop method
+def print_fib(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a,b = 0, 1
+        for _ in range(2, n):
+            a, b = b , a+b
+        return b
+
+
+def get_fib_series(n):
+    if n ==1:
+        return [0]
+    elif n ==2:
+        return [0,1]
+    else:
+        ser = [0,1]
+        for i in range(2, n):
+            ser.append(ser[i-2] + ser[i-1])
+        return ser
+
+
+print(print_fib(4))
+print(get_fib_series(5))
+print(get_fib_series(1))
+print(get_fib_series(2))
