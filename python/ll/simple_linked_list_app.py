201ca0eb84030db9ba1ab41a070e72a60950dc75
beercafeguy <hemchandra@outlook.com>
2025-06-15 20:53:44 +0530

    Moved linked list pointers for middle insertion

--- python/ll/simple_linked_list_app.py
+++ python/ll/simple_linked_list_app.py
@@ -9,6 +9,7 @@ class SimpleLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
     
     def insert(self, value):
         node = LinkedListNode(value)
@@ -18,20 +19,54 @@ class SimpleLinkedList:
         else:
             self.tail.next = node
             self.tail = node
+        self.length +=1
+    
+    def insert_at_start(self, value):
+        new_node = LinkedListNode(value)
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+    
+    def insert_at_given_position(self, position, value):
+        if position > self.length:
+            return None
+        if position == 0:
+            self.insert_at_start(value)
+        else:
+
+            new_node = LinkedListNode(value)
+            current = self.head
+            i = 0
+            while i < position -1:
+                current = current.next
+                i+=1
+            new_node.next = current.next
+            current.next = new_node
+        self.length+=1
+
+
     
     def print(self):
+        print(self.get_as_py_list())
 
+    def get_as_py_list(self):
         current = self.head
-
-        while current != None:
-            print(current.value)
+        result = []
+        while current:
+            result.append(current.value)
             current = current.next
+        return result
+    
+
 
 
 def main():
     ll = SimpleLinkedList()
     for i in range(1, 10):
         ll.insert(i)
+    
+    ll.insert_at_start(12)
+    ll.insert_at_given_position(3, 34)
     
     ll.print()
 
