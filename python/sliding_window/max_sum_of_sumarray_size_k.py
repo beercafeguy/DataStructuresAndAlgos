b3cfe2b46621b188a6af1921fe82a29a0a31b918
beercafeguy <hemchandra@outlook.com>
2025-06-16 22:35:52 +0530

    practice

--- python/sliding_window/max_sum_of_sumarray_size_k.py
+++ python/sliding_window/max_sum_of_sumarray_size_k.py
@@ -10,10 +10,20 @@ def max_sum_subarray(arr, k):
             max_sum = max(max_sum, window_sum)
         return max_sum
 
+def max_sum_subarray_v1(arr, k):
+    if len(arr) < k:
+        return None
+    else:
+        window_sum = sum(arr[:k])
+        max_sum = window_sum
+        for i in range(k, len(arr)):
+            window_sum = window_sum + arr[i] - arr[i-k]
+            max_sum = max(max_sum, window_sum)
+        return max_sum
 
 arr = [2, 1, 5, 1, 3, 2]
 k = 3
 
-print(max_sum_subarray(arr, k))
+print(max_sum_subarray_v1(arr, k))
 
 # This is a fixed length sliding window problem
