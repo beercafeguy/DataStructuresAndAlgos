85570ba4e6235e52426ed504b2b90b7618ac0538
beercafeguy <hemchandra@outlook.com>
2025-06-19 22:09:34 +0530

    Check anagram issue resolved

---
+++ python/linear/twosum_problem.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) < 2:
+            return []
+        else:
+            i = 0
+            j = 1
+            for j in range(len(nums)):
+                
+
+
+
+sol = Solution()
+sol.twoSum([1,3,8,12], 11)
---
+++ python/string/check_anagram.py
@@ -0,0 +1,19 @@
+
+def are_anagram(str1, str2):
+    holder = dict()
+    for ch in str1:
+        if ch in holder:
+            holder[ch] +=1
+        else:
+            holder[ch] = 1
+        
+    for ch in str2:
+        if ch not in holder:
+            return False
+        holder[ch] = holder[ch] -1
+        if holder[ch] < 0:
+            return False
+    return True 
+
+
+print(are_anagram('and','dnas'))
